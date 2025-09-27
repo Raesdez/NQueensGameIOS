40fe20fe43b765577a9842c5e165c9743b49449f
Ramón <ramon.sosa.fernandez@gmail.com>
2025-09-27 19:37:30 +0200

    Initial Commit

---
+++ NQueensGame/NQueensGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensGameApp.swift
+//  NQueensGame
+//
+//  Created by Ramón on 27/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NQueensGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NQueensGameTests/NQueensGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensGameTests.swift
+//  NQueensGameTests
+//
+//  Created by Ramón on 27/9/25.
+//
+
+import Testing
+@testable import NQueensGame
+
+struct NQueensGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
