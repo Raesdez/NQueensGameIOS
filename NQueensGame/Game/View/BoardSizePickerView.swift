01208c25e1cf921a097de9e6c1c0672419d6f1fd
Ramón <ramon.sosa.fernandez@gmail.com>
2025-09-28 03:24:22 +0200

    First commit, base logic and MVVM

---
+++ NQueensGame/Game/View/BoardSizePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  BoardSizePickerView.swift
+//  NQueensGame
+//
+//  Created by Ramón on 28/9/25.
+//
+
+import SwiftUI
+
+struct BoardSizePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoardSizePickerView()
+}
---
+++ NQueensGame/Game/View/GameBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  GameBoardView.swift
+//  NQueensGame
+//
+//  Created by Ramón on 28/9/25.
+//
+
+import SwiftUI
+
+struct GameBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameBoardView()
+}
